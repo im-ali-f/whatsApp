cfa2d13f95a02b9a0758e9a7282f41c91b40ae06
Ali <alifarhad.work@gmail.com>
2024-05-29 14:20:12 +0330

    upper row

--- app/src/main/java/com/example/whatsapp/HomePage/ChatsComp.kt
+++ app/src/main/java/com/example/whatsapp/HomePage/ChatsComp.kt
@@ -24,12 +24,15 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.whatsapp.R
 import com.example.whatsapp.VMs.App.WhatsAppVM
 import com.example.whatsapp.ui.theme.disabledText
+import com.example.whatsapp.ui.theme.enabledText
+import com.example.whatsapp.ui.theme.mainBlue
 import com.example.whatsapp.ui.theme.mainGray
 import com.example.whatsapp.ui.theme.mainMessageTextColor
 import com.example.whatsapp.ui.theme.messageIconColor
@@ -37,136 +40,165 @@ import com.example.whatsapp.ui.theme.messageIconColor
 @Composable
 fun ChatsComp(model: WhatsAppVM) {
 
-    //test Row
-    Column(modifier = Modifier.fillMaxWidth()) {
 
-
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .clickable { }
-                .padding(start = 16.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween) {
-
-            Box(
+    Column(Modifier.fillMaxSize()) {
+        Row (Modifier.fillMaxWidth().padding(start = 11.dp , end = 16.dp , top = 5.dp, bottom = 5.dp), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween){
+            Text(
                 modifier = Modifier
-                    .size(52.dp)
-                    .clip(RoundedCornerShape(100))
-                    .background(Color.LightGray)
+                    .clip(RoundedCornerShape(8.dp))
+                    .clickable { }
+                    .padding(5.dp),
+                text = "Broadcast Lists",
+                fontWeight = FontWeight(400),
+                fontSize = 17.sp,
+                textAlign = TextAlign.Center,
+                color = mainBlue
             )
-            {
-                //inja image ezafe mishe
-            }
 
-            Column(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(start = 12.dp), verticalArrangement = Arrangement.SpaceBetween
-            ) {
-                //sep
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(1.dp)
-                        .background(disabledText)
-                )
-                Spacer(modifier = Modifier.height(10.dp))
-                //endsep
-
-                Row(
-                    Modifier
-                        .fillMaxWidth()
-                        .height(75.dp), verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Column(
-                        Modifier
-                            .fillMaxHeight()
-                            .fillMaxWidth(0.9f)
-                            .padding(start = 2.dp, end = 10.dp)
-                        //.background(Color.Black)
-                    ) {
-
-                        Row(
-                            Modifier.fillMaxWidth(),
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.SpaceBetween
-                        ) {
-                            Text(
-                                text = "Ali Farhad",
-                                fontWeight = FontWeight(600),
-                                fontSize = 18.sp,
-                                color = Color.Black
-
-                            )
-
-                            Text(
-                                text = "11/12/2024",
-                                fontWeight = FontWeight(400),
-                                fontSize = 15.sp,
-                                color = mainMessageTextColor
-
-                            )
-
-
-                        }
-
-                        Row(
-                            Modifier.fillMaxSize(),
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.Center
-                        ) {
-                            Text(
-                                modifier = Modifier.fillMaxWidth(),
-                                maxLines = 2,
-                                overflow = TextOverflow.Ellipsis,
-                                text = "hey whts up ? lemmi think about something new that can change every thing. what is your idea?",
-                                fontWeight = FontWeight(600),
-                                fontSize = 14.sp,
-                                color = mainMessageTextColor
-
-                            )
-
-
-                        }
-
-                    }
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                        //  .background(Color.Yellow)
-                    ) {
-                        Icon(
-                            modifier = Modifier
-                                .size(20.dp)
-                                .align(Alignment.CenterStart),
-                            painter = painterResource(id = R.drawable.shape),
-                            tint = messageIconColor,
-                            contentDescription = "chat Icon"
-                        )
-                    }
-                }
-
-
-                //sep
-                Spacer(modifier = Modifier.height(10.dp))
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(1.dp)
-                        .background(disabledText)
-                )
-
-                //end sep
-
-            }
-
+            Text(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(8.dp))
+                    .clickable { }
+                    .padding(5.dp),
+                text = "New Group",
+                fontWeight = FontWeight(400),
+                fontSize = 17.sp,
+                textAlign = TextAlign.Center,
+                color = mainBlue
+            )
         }
 
+    }
+
+
+}
+
+@Composable
+fun chatBuilder(){
+    //test Row
+
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .clickable { }
+            .padding(start = 16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween) {
+
+        Box(
+            modifier = Modifier
+                .size(52.dp)
+                .clip(RoundedCornerShape(100))
+                .background(Color.LightGray)
+        )
+        {
+            //inja image ezafe mishe
+        }
+
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .padding(start = 12.dp), verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            //sep
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(disabledText)
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            //endsep
+
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .height(75.dp), verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    Modifier
+                        .fillMaxHeight()
+                        .fillMaxWidth(0.9f)
+                        .padding(start = 2.dp, end = 10.dp)
+                    //.background(Color.Black)
+                ) {
+
+                    Row(
+                        Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "Ali Farhad",
+                            fontWeight = FontWeight(600),
+                            fontSize = 18.sp,
+                            color = Color.Black
+
+                        )
+
+                        Text(
+                            text = "11/12/2024",
+                            fontWeight = FontWeight(400),
+                            fontSize = 15.sp,
+                            color = mainMessageTextColor
+
+                        )
+
+
+                    }
+
+                    Row(
+                        Modifier.fillMaxSize(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis,
+                            text = "hey whts up ? lemmi think about something new that can change every thing. what is your idea?",
+                            fontWeight = FontWeight(600),
+                            fontSize = 14.sp,
+                            color = mainMessageTextColor
+
+                        )
+
+
+                    }
+
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                    //  .background(Color.Yellow)
+                ) {
+                    Icon(
+                        modifier = Modifier
+                            .size(20.dp)
+                            .align(Alignment.CenterStart),
+                        painter = painterResource(id = R.drawable.shape),
+                        tint = messageIconColor,
+                        contentDescription = "chat Icon"
+                    )
+                }
+            }
+
+
+            //sep
+            Spacer(modifier = Modifier.height(10.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(disabledText)
+            )
+
+            //end sep
+
+        }
 
     }
 
 
     //test Row
-
 }
